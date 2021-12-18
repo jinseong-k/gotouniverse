7ad52ca7f91eaedc9d4c2d5f39dfdd52aa2af73d
Jinseong Kim <nadas9029@gmail.com>
2021-12-18 11:38:48 +0900

    Update Basic Socket test program

--- clang/src/network/src/tcp_socket_server.c
+++ clang/src/network/src/tcp_socket_server.c
@@ -3,13 +3,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
-
-#include <pthread.h>
-
 #include <errno.h>
+
+#include "tcp_socket_server.h"
 
 uint32_t create_socket() {
   int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
@@ -60,6 +56,16 @@ void *send_proc(void *_sock) {
   }
 }
 
+void run_proc(void *_server_struct) {
+  SERVER_STRUCT *server_struct;
+  int thread_join;
+
+  server_struct = (SERVER_STRUCT *)_server_struct;
+
+  pthread_create(&server_struct->sender, NULL, send_proc, (void *)&server_struct->socket_fd);
+  pthread_create(&server_struct->receiver, NULL, recv_proc, (void *)&server_struct->socket_fd);
+}
+
 int main() {
   int socket_fd, new_socket;
   int ret;
@@ -67,10 +73,10 @@ int main() {
   int len = sizeof(struct sockaddr);
   int port = 9001;
 
-  pthread_t recv_thread;
-  pthread_t send_thread;
+  int idx = 0;
+  SERVER_STRUCT server_struct[16];
+  pthread_t worker_thread[16];
 
-  int thread_join;
 
   socket_fd = create_socket();
   if (socket_fd < 0) {
@@ -90,23 +96,35 @@ int main() {
     return -1;
   }
 
-  while(1) {
-    if(listen(socket_fd, 16) < 0) {
+  while (1) {
+    fprintf(stderr, "[%s:%d]\n", __func__, __LINE__);
+    if (idx >= 16) {
+      continue;
+    }
+
+    if (listen(socket_fd, 16) < 0) {
       fprintf(stderr, "[%s:%d] listen error\n", __func__, __LINE__);
       return -1;
     }
 
     new_socket = accept(socket_fd, (struct sockaddr *)&address, (socklen_t *)&len);
+    fprintf(stderr, "[%s:%d] Connect Client Info [%s:%u]\n",
+        __func__, __LINE__, inet_ntoa(address.sin_addr), ntohs(address.sin_port));
     if (new_socket < 0) {
       fprintf(stderr, "[%s:%d] accept error\n", __func__, __LINE__);
       return -1;
     }
 
-    pthread_create(&recv_thread, NULL, recv_proc, (void *)&new_socket);
-    pthread_create(&send_thread, NULL, send_proc, (void *)&new_socket);
+    server_struct[idx].client_addr = address.sin_addr.s_addr;
+    server_struct[idx].client_port = address.sin_port;
+    server_struct[idx].socket_fd = new_socket;
+    server_struct[idx].idx = idx;
 
-    pthread_join(recv_thread, (void **)&thread_join);
-    pthread_join(send_thread, (void **)&thread_join);
+    run_proc(&server_struct[idx]);
+    //pthread_create(&worker_thread[idx], NULL, run_proc, (void *)&server_struct[idx]);
+
+    idx++;
   }
 
+  return 1;
 }
